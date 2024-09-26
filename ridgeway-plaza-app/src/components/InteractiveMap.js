65b2d0cddb9e0911bb9e5c049a4c44c03d63921f
Choudhry-Shaharyar <sheary009@hotmail.com>
2024-09-25 21:59:24 -0400

    added new store

--- ridgeway-plaza-app/src/components/InteractiveMap.js
+++ ridgeway-plaza-app/src/components/InteractiveMap.js
@@ -32,18 +32,36 @@ const InteractiveMap = () => {
         className='mapIframe'
         data-src="https://maker.mappedin.com/map/6570dfeafd001d626819c691"
         style={{ border: '2px #8080801a solid' }}
-        name="myiFrame"
+        name="Mappedin Map"
         loading="lazy"  // Native lazy loading
         scrolling="no"
-        frameBorder="1"
+        frameBorder="0"
         marginHeight="0"
         marginWidth="0"
         title="Interactive map of Ridgeway Plaza"
-        height="400px"
-        width="600px"
+        height="450px"
+        width="100%"
         allowFullScreen
+        allow="clipboard-write; web-share"
         metaname="description" content="Explore Ridgeway Plaza with our interactive map! Navigate through our diverse range of shops, restaurants, and services. Find your way around easily and discover everything Ridgeway Plaza has to offer."
       ></iframe>
+    {/* <iframe
+      className='mapIframe'
+      src="https://app.mappedin.com/map/6570dfeafd001d626819c691?embedded=true"
+      style={{ border: '2px #8080801a solid' }}
+      name="Ridgeway Plaza"
+      loading="lazy"  // Native lazy loading
+      scrolling="no"
+      frameBorder="0"
+      marginHeight="0"
+      marginWidth="0"
+      title="Mappedin Map"
+      height="650px"
+      width="100%"
+      allowFullScreen
+      //allow="clipboard-write; web-share"
+      metaname="description" content="Explore Ridgeway Plaza with our interactive map! Navigate through our diverse range of shops, restaurants, and services. Find your way around easily and discover everything Ridgeway Plaza has to offer."
+    ></iframe> */}
     </div>
   );
 }
